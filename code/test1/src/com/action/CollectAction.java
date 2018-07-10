3358b4c6619003f8c17220291ec86e934e0b7338
Aydous <31283594+Aydous@users.noreply.github.com>
2018-07-10 19:06:25 +0800

    收藏

---
+++ code/test1/src/com/action/CollectAction.java
@@ -0,0 +1,54 @@
+package com.action;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.dao.CollectDao;
+
+/**
+ * Servlet implementation class CollectAction
+ */
+
+public class CollectAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CollectAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String uid = (String) session.getAttribute("userid");
+		String vid = (String) session.getAttribute("videoid");
+		CollectDao cd=new CollectDao();
+		if(cd.collect(uid, vid)) {
+			request.setAttribute("result", "³É¹¦");
+			request.getRequestDispatcher("xx.jsp").forward(request, response);
+		}else {
+			request.setAttribute("result", "Ê§°Ü");
+			request.getRequestDispatcher("xx.jsp").forward(request, response);
+		}
+	}
+
+}
--- code/test1/src/com/dao/FileDao.java
+++ code/test1/src/com/dao/FileDao.java
@@ -4,7 +4,6 @@ import java.io.*;
 import com.comm.BaseDao;
 import com.pojo.*;
 public class FileDao extends BaseDao {
-	String sql="INSERT INTO `schoolol`.`video` (`userid`,`title`,`introduce`,`view`,`file`) VALUES (?,?,?,0,?)";
 	public  boolean Upload(Video v) {
 		String sql="INSERT INTO `schoolol`.`video` (`userid`,`title`,`introduce`,`view`,`file`) VALUES (?,?,?,0,?)";
 		Object [] obs= {v.getUserId(),v.getTitle(),v.getIntroduce(),v.getFile()};
