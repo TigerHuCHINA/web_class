0575e733d88c2679401acc3a1b531aff9b31a1e4
tigerhu <845206437@qq.com>
2018-07-12 09:24:10 +0800

    2

--- code/test1/src/com/action/FollowAction.java
+++ code/test1/src/com/action/FollowAction.java
@@ -8,20 +8,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.dao.CollectDao;
+import com.dao.FollowDao;
+import com.pojo.Follow;
 
-public class disAgreeAction extends HttpServlet {
+public class FollowAction extends HttpServlet {
 	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
 		doPost(request, response);
 	}
 	protected void doPost(HttpServletRequest request,HttpServletResponse response)throws IOException, ServletException {
 		HttpSession session = request.getSession();
-		String uid = (String)session.getAttribute("userid");
-		String cid = (String)session.getAttribute("commentid");
-		AgreeDao a = new AgreeDao();
-		if(a.disagree(uid,cid)) {
+		Follow ff = new Follow();
+		FollowDao f = new FollowDao();
+		if(f.adFollow(ff)) {
 			request.setAttribute("result", "³É¹¦");
-			request.getRequestDispatcher("video.jsp").forward(request, response);
+			request.getRequestDispatcher("homePage.jsp").forward(request, response);
 		}else {
 			request.setAttribute("result", "Ê§°Ü");
 			request.getRequestDispatcher("xx.jsp").forward(request, response);
---
+++ code/test1/src/com/action/delAgreeAction.java
@@ -0,0 +1,30 @@
+package com.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.dao.AgreeDao;
+import com.pojo.Agree;
+
+public class delAgreeAction extends HttpServlet {
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
+		doPost(request, response);
+	}
+	protected void doPost(HttpServletRequest request,HttpServletResponse response)throws IOException, ServletException {
+		HttpSession session = request.getSession();
+		Agree aa = new Agree();
+		AgreeDao a = new AgreeDao();
+		if(a.delAgree(aa)) {
+			request.setAttribute("result", "³É¹¦");
+			request.getRequestDispatcher("video.jsp").forward(request, response);
+		}else {
+			request.setAttribute("result", "Ê§°Ü");
+			request.getRequestDispatcher("xx.jsp").forward(request, response);
+		}
+	}
+}
