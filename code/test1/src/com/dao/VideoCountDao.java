7f1fb3ce66257680f9dc589f62737eb45a3e8f84
Duyuhang <461514832@qq.com>
2018-07-16 18:30:59 +0800

    修改浏览bug

--- code/test1/src/com/dao/VideoCountDao.java
+++ code/test1/src/com/dao/VideoCountDao.java
@@ -10,9 +10,8 @@ public class VideoCountDao extends BaseDao {
 		//String sql="if exists(select from video where userId=? and videoId=?)"
 		//		+ "update video set view=view+1 where userId=? and videoId=?"
 		//		+ "else insert into video(userId,videoId,view) values(?,?,1)";
-		String sql="update video set view=view+1 where userId=? and Id=? and \r\n" + 
-				"time_to_sec(now())-(select max(time_to_sec(daytime)) as d1 from videorecord where userId=? and videoId=?)>300;";
-		Object[] obs= {u.getUid(),v.getId(),u.getUid(),v.getId()};
+		String sql="update video set view=view+1 where userId=? and Id=? and time_to_sec(now())-(select max(time_to_sec(daytime)) as d1 from videorecord where userId=? and videoId=?)>300";
+		Object[] obs= {u.getUid(),v.getId(),u.getUid(),v.getId(),u.getUid(),v.getId()};
 		
 		int row = executeUpdate(sql,obs);
 		free();
