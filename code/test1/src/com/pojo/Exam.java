d2b3fca133d2e77b6bd1cfa472ab90203fc17b25
tigerhu <845206437@qq.com>
2018-07-17 20:59:12 +0800

    添加题目1

--- code/test1/src/com/pojo/Exam.java
+++ code/test1/src/com/pojo/Exam.java
@@ -4,12 +4,8 @@ public class Exam {
 	private int examId;
 	private String title;
 	private String time;
-	private int qId;
-	private String qContent;
-	private int empty;
-	private int aId;
-	private String aContent;
-	private int aScore;
+	
+	
 	public int getExamId() {
 		return examId;
 	}
@@ -27,41 +23,5 @@ public class Exam {
 	}
 	public void setTime(String time) {
 		this.time = time;
-	}
-	public int getqId() {
-		return qId;
-	}
-	public void setqId(int qId) {
-		this.qId = qId;
-	}
-	public String getqContent() {
-		return qContent;
-	}
-	public void setqContent(String qContent) {
-		this.qContent = qContent;
-	}
-	public int getEmpty() {
-		return empty;
-	}
-	public void setEmpty(int empty) {
-		this.empty = empty;
-	}
-	public int getaId() {
-		return aId;
-	}
-	public void setaId(int aId) {
-		this.aId = aId;
-	}
-	public String getaContent() {
-		return aContent;
-	}
-	public void setaContent(String aContent) {
-		this.aContent = aContent;
-	}
-	public int getaScore() {
-		return aScore;
-	}
-	public void setaScore(int aScore) {
-		this.aScore = aScore;
 	}
 }
